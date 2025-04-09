a1c59a783ea173a2a3b5351aa0bee79bc903e33b
Samuel Howard <sahoward42@tntech.edu>
2025-04-09 16:54:29 -0500

    Add grade break to grade figure creation

--- make_figures/grade_vs_mpg.py
+++ make_figures/grade_vs_mpg.py
@@ -21,7 +21,7 @@ def make_grade_mpg_plot():
     # Round the grade to the nearest % for grouping
     data["Grade"] = np.round(data["Grade"], decimals=2)
 
-    # Group by speed and compute average MPG
+    # Group by grade and compute average MPG
     grouped = data.groupby("Grade")["Miles Per Gallon(Instant)(mpg)"].mean()
 
     basic_plot(
@@ -32,4 +32,26 @@ def make_grade_mpg_plot():
         "Average Miles Per Gallon (mpg)",
         "Grade (Δft/ft) vs. MPG",
         "figures/grade_vs_mpg.png"
+    )
+
+    data = load_data()
+    data = data[["Grade Break", "Miles Per Gallon(Instant)(mpg)"]]
+    data = data.apply(pd.to_numeric, errors='coerce')
+    data = data.dropna()
+    data = data[data["Miles Per Gallon(Instant)(mpg)"] < 512]
+
+    # Round the grade break to the nearest % for grouping
+    data["Grade Break"] = np.round(data["Grade Break"], decimals=2)
+
+    # Group by grade break and compute average MPG
+    grouped = data.groupby("Grade Break")["Miles Per Gallon(Instant)(mpg)"].mean()
+
+    basic_plot(
+        grouped.index,
+        grouped.values,
+        "Average MPG",
+        "Grade Break (Δft/ft/s)",
+        "Average Miles Per Gallon (mpg)",
+        "Grade Break (Δft/ft/s) vs. MPG",
+        "figures/grade_break_vs_mpg.png"
     )
